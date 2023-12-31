cf5ce659b195c5b4b6a745ac5bec67dcb28af349
fikiaprian23 <fikiaprian23@gmail.com>
2023-12-31 12:17:07 +0700

    add taildwindcss

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FikiAp || About</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="text-3xl font-bold">Ini halaman about</h1>
+    <h3 class="text-xl"> Fiki Aprian</h3>
+    <p class="text-lg"> fikiaprian23@gmail.com</p>
+    <img src="img/fiki.png" alt="fiki" width="200">
+</body>
+</html>
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>FikiAp || blog</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>Ini halaman blog</h1>
+    <h2>Ini halaman blog</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, aut? Harum, reprehenderit dolorum. Aliquid, nesciunt officiis? Quasi placeat voluptatum, aspernatur consequuntur corrupti natus dignissimos accusantium, perspiciatis ipsum explicabo cupiditate est.</p>
+</body>
+</html>
