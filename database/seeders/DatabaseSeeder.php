0a9c7de754960c9692ed468fd4246445bc82fe8c
fikiaprian23 <fikiaprian23@gmail.com>
2024-01-08 12:42:53 +0700

    add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +17,37 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('12345'),
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming',
+        ]);
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal',
+        ]);
+
+        Post::create([
+            'title' => 'Post kedua',
+            'user_id' => 1,
+            'category_id' => 2,
+            'slug' => 'post-kedua',
+            'excerpt' => 'Tentang kamu',
+            'body' => 'Barangkali selama ini aku terlalu percaya diri. Meyakini bahwa kau juga punyai rasa yang sama denganku. Padahal bisa saja balasan pesanmu itu hanya sebagai bentuk caramu untuk menghargaiku. Atau bisa juga saat itu kau memang sedang luang untuk aku yang tiba-tiba datang di teras penantianmu; menanti seseorang yang jelas itu bukan aku. Kupikir aku benar-benar bisa untuk tidak berharap kepadamu. Namun ternyata aku masih belum bisa sampai pada tahap itu. Imajinasiku terlalu liar tentang hubungan yang nantinya dapat kita cipta bersama. Aku terlalu perasa, namun untuk kali ini saja. Sial! Saat ini aku benar-benar sedang jatuh hati pada seorang gadis baik hati. Tak biasanya seperti ini; menyukai seseorang untuk waktu yang begitu panjang. Oh, Tuhan, aku pasrah akan segala yang kau tentukan. Memang seharusnya hanya kepadamu aku gantungkan.',
+        ]);
+
+        Post::create([
+            'title' => 'Post ketiga',
+            'category_id' => 1,
+            'user_id' => 1,
+            'slug' => 'post-ketiga',
+            'excerpt' => 'Membuat website',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, temporibus animi, soluta distinctio, ut dignissimos aliquam fugiat eligendi nemo asperiores in laboriosam repudiandae libero minima odit aliquid corporis consequuntur nihil.',
+        ]);
     }
 }
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,13 +2,17 @@
 @extends('layouts.main')
 
 @section('container')
+    <h1 class="text-5xl mb-10 text-center font-bold">Posts</h1>
     @foreach ($posts as $post)
-        <article class="mb-10">
-            <h2 class="font-bold text-2xl">
+        <article class="mb-10 border-b pb-4">
+            <h2 class="font-bold text-2xl text-blue-600">
                 <a href="/blog/{{ $post->slug }}">{{ $post->title }}</a>
             </h2>
+            <p>Posted by: Fiki Aprian in <a href="/categories/{{ $post->category->slug }}"
+                    class="text-blue-500">{{ $post->category->name }}</a></p>
 
             <p>{{ $post->excerpt }}</p>
+            <a href="/blog/{{ $post->slug }}" class="text-blue-500 underline">Read more...</a>
         </article>
     @endforeach
 @endsection
