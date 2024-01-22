f3f14877caf67745ee2fdf2f9b6d93354c04cf64
fikiaprian23 <fikiaprian23@gmail.com>
2024-01-22 20:06:27 +0700

    add post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,18 +1,34 @@
-{{-- @dd($posts) --}}
 @extends('layouts.main')
 
 @section('container')
-    <article class="mb-10">
-        <h2 class="font-bold text-2xl">
-            {{ $post->title }}
-        </h2>
+    <div class="container mx-auto my-8">
+        <div class="max-w-2xl mx-auto">
+            <h2 class="font-bold text-2xl mb-3">
+                {{ $post->title }}
+            </h2>
 
-        <p>Posted by: <a href="/author/{{ $post->author->username }}"
-                class="text-blue-500 underline">{{ $post->author->name }}</a> in <a
-                href="/categories/{{ $post->category->slug }}" class="text-blue-500">{{ $post->category->name }}</a></p>
+            <p class="text-gray-600">
+                Posted by:
+                <a href="/author/{{ $post->author->username }}" class="text-blue-500 underline">
+                    {{ $post->author->name }}
+                </a>
+                in
+                <a href="/categories/{{ $post->category->slug }}" class="text-blue-500">
+                    {{ $post->category->name }}
+                </a>
+            </p>
 
-        {!! $post->body !!}
-    </article>
+            <img class="rounded-t-lg" src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="" />
 
-    <a href="/blog">Back to blog</a>
+            <article class="mt-3">
+                {!! $post->body !!}
+            </article>
+
+
+
+            <a href="/blog" class="text-blue-500 mt-4 inline-block">
+                Back to blog
+            </a>
+        </div>
+    </div>
 @endsection
