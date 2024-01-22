ab679ea4bbd2502a17dbda9e648fd5d5615bb31f
fikiaprian23 <fikiaprian23@gmail.com>
2024-01-22 20:22:20 +0700

    fix nav

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -37,15 +37,19 @@
                 <div class="hidden sm:ml-6 sm:block">
                     <div class="flex space-x-4">
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                        <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
+                        <a href="/"
+                            class="bg-gray-900 {{ request()->is('/') || request()->is('') ? 'text-white' : 'text-gray-300' }} rounded-md px-3 py-2 text-sm font-medium"
                             aria-current="page">Home</a>
                         <a href="/blog"
-                            class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Blog</a>
+                            class="{{ request()->is('blog') ? 'text-white' : 'text-gray-300' }} hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Blog</a>
+                        <a href="/categories"
+                            class="{{ request()->is('categories') ? 'text-white' : 'text-gray-300' }} hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Categories</a>
                         <a href="/about"
-                            class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About
+                            class="{{ request()->is('about') ? 'text-white' : 'text-gray-300' }} hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About
                             me</a>
                         <a href="/contact"
-                            class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+                            class="{{ request()->is('contact') ? 'text-white' : 'text-gray-300' }} hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+
                     </div>
                 </div>
             </div>
@@ -73,22 +77,7 @@
                         </button>
                     </div>
 
-                    <!--
-            Dropdown menu, show/hide based on menu state.
 
-            Entering: "transition ease-out duration-100"
-              From: "transform opacity-0 scale-95"
-              To: "transform opacity-100 scale-100"
-            Leaving: "transition ease-in duration-75"
-              From: "transform opacity-100 scale-100"
-              To: "transform opacity-0 scale-95"
-          -->
-                    {{-- <div class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-            <!-- Active: "bg-gray-100", Not Active: "" -->
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
-          </div> --}}
                 </div>
             </div>
         </div>
